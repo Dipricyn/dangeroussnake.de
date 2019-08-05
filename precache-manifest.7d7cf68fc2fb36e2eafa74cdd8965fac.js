f398312c27d36a0772dcb9098a1dea07b58ae436
Dipricyn <leer.add@web.de>
2019-08-05 18:56:37 +0200

    Updates

---
+++ precache-manifest.7d7cf68fc2fb36e2eafa74cdd8965fac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b242245f4e9ceb7dbbdf9de905d8f191",
+    "url": "/dangeroussnake.de/index.html"
+  },
+  {
+    "revision": "07f74873b43900c43137",
+    "url": "/dangeroussnake.de/static/css/main.3d4c03cb.chunk.css"
+  },
+  {
+    "revision": "03c5fd4548e363fd6bed",
+    "url": "/dangeroussnake.de/static/js/2.f6a60ff5.chunk.js"
+  },
+  {
+    "revision": "07f74873b43900c43137",
+    "url": "/dangeroussnake.de/static/js/main.27a12a80.chunk.js"
+  },
+  {
+    "revision": "4c5c876f3e91e4bc247f",
+    "url": "/dangeroussnake.de/static/js/runtime~main.d280ea42.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6c52496b3f65baaa2cfbaa0a54f6500a.js"
+  "/dangeroussnake.de/precache-manifest.7d7cf68fc2fb36e2eafa74cdd8965fac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dangeroussnake.de/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
